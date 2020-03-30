0b2e57f10413ab11a17dd28d0c3c88fdb03cb097
Ramon <ramonomello@gmail.com>
2020-03-30 02:45:50 -0300

    add logon page

---
+++ frontend/src/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header>
+            <h1>Bew the hero</h1>
+        </header>
+    )
+}
---
+++ frontend/src/pages/Logon/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { FiLogIn } from 'react-icons/fi';
+
+import './styles.css';
+
+import logoImg from '../../assets/logo.svg';
+import heroesImg from '../../assets/heroes.png'
+
+export default function Logon() {
+    return (
+        <dev className="logon-container">
+            <section className="form">
+                <img src={logoImg} alt="be the hero"/>
+
+                <form>
+                    <h1>Faça seu logon</h1>
+
+                    <input placeholder="Sua ID" />
+                    <button className = "button" type="submit">Entrar</button>
+
+                    <a href="/register">
+                        <FiLogIn size={16} color="#e02041" />
+                        Não tenho cadastro
+                    </a>
+                </form>
+            </section>
+
+            <img src={heroesImg} alt="logo Heroes"/>
+        </dev>
+    )
+}
